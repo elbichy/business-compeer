d2700597f778f28b047a0829ccfc3448a7b9a0b1
Suleiman Abdulrazaq <suleiman.bichi@gmail.com>
2019-05-06 14:30:36 +0100

    Arranging my routes

--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,41 @@ Route::get('/', 'PagesController@index');
 
 Auth::routes();
 
+Route::post( 'switchBranch', 'ProcessBusiness@switchBranch');
+
 Route::get('/dashboard', 'DashboardController@index');
+
 Route::get('/Dashboard/sales', 'DashboardController@sales');
+Route::get( 'Dashboard/getReciept/{data}', 'Transactions@getReciept');
+Route::post( 'storeSales', 'Transactions@storeSales');
 Route::put( 'Dashboard/clearOutstanding/{data}', 'Transactions@clearOutstanding');
 Route::delete( 'Dashboard/deleteSale/{data}', 'Transactions@deleteSale');
 
 Route::get('/Dashboard/expenses', 'DashboardController@expenses');
+Route::post( 'storeExpenses', 'Transactions@storeExpenses');
 Route::delete( 'Dashboard/deleteExpense/{data}', 'Transactions@deleteExpense');
 
 Route::get('/Dashboard/stock', 'DashboardController@stock');
+Route::post( 'storeStock', 'Transactions@storeStock');
 Route::delete( 'Dashboard/deleteStock/{data}', 'Transactions@deleteStock');
 
 Route::get('/Dashboard/statistics', 'DashboardController@statistics');
 Route::get('/Dashboard/customers', 'DashboardController@customers');
 
 Route::get( '/Dashboard/businessSettings', 'DashboardController@businessSettings');
-Route::get( '/Dashboard/branchSettings', 'DashboardController@branchSettings');
-Route::get( '/Dashboard/staffSettings', 'DashboardController@staffSettings');
-Route::get( 'Dashboard/getReciept/{data}', 'Transactions@getReciept');
-
-Route::post( 'storeSales', 'Transactions@storeSales');
-Route::post( 'storeExpenses', 'Transactions@storeExpenses');
-Route::post( 'storeStock', 'Transactions@storeStock');
 Route::post( 'storeBusinessSettings', 'ProcessBusiness@businessSettings');
+
+Route::get( '/Dashboard/branchSettings', 'DashboardController@branchSettings');
 Route::post( 'storeBranchSettings', 'ProcessBusiness@branchSettings');
+
+Route::get( '/Dashboard/staffSettings', 'DashboardController@staffSettings');
 Route::post( 'storeStaffSettings', 'ProcessBusiness@staffSettings');
-Route::post( 'switchBranch', 'ProcessBusiness@switchBranch');
+
+
+
+
+
+
+
+
 
